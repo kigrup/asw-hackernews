50b0d3f880294393d7aab78944e0a9cf608f2749
Raul Plesa <raul.plesa@estudiantat.upc.edu>
2021-10-20 17:11:54 +0200

    Fixed logger

--- utils/logger.js
+++ utils/logger.js
@@ -1,15 +1,8 @@
 const winston = require('winston');
 const moment = require('moment');
 
-const myFormat = printf(({ level, message, timestamp, ...metadata }) => {
-    let msg = `${timestamp} [${level}] : ${message} `;
-    if (metadata) {
-        msg += JSON.stringify(metadata);
-    }
-    return msg;
-});
-
 const logger = winston.createLogger({
+    level: 'info',
     format: winston.format.json(),
     defaultMeta: { service: 'server' },
     transports: [
