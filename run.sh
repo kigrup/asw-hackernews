26b32f67fc706c911834f309ac589b4bf83725da
kigrup <raul.plesa@gmail.com>
2021-10-20 12:33:46 +0200

    Improved run script

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-docker build -t raulplesa/hackernews .
+docker build --rm -t raulplesa/hackernews .
+docker stop hackernews
+echo y | docker system prune
 docker run -d -p 13001:13001 --name hackernews raulplesa/hackernews
