64727278111837e0f638430a83f4344edfbde1bd
Raul Plesa <raul.plesa@estudiantat.upc.edu>
2021-10-20 15:33:21 +0200

    Fixed log file path

--- utils/logger.js
+++ utils/logger.js
@@ -3,7 +3,9 @@ const logger = winston.createLogger({
     level: 'http',
     format: winston.format.json(),
     defaultMeta: { service: 'server' },
-    transports: [new winston.transports.File({ filename: Date.now() })],
+    transports: [
+        new winston.transports.File({ filename: Date.now().toISOString() }),
+    ],
 });
 
 module.exports = logger;
