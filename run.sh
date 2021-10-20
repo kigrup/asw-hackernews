d03cb8432e016932d43e7171ea0a41f034ad55fd
kigrup <raul.plesa@gmail.com>
2021-10-20 13:28:39 +0200

    Moved database to volume

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 docker build --rm -t raulplesa/hackernews .
 docker stop hackernews
 echo y | docker system prune
-docker run -d -p 13001:13001 --name hackernews raulplesa/hackernews
+docker run -d -p 13001:13001 --name hackernews -v ./db/data:./db/data raulplesa/hackernews
