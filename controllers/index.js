7fd3d27c7f0a54add2b64aea288303c024ca0f63
Raul Plesa <raul.plesa@estudiantat.upc.edu>
2021-10-20 12:56:58 +0200

    Added posts to homepage

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,25 @@
 const { StatusCodes } = require('http-status-codes');
+const Contribution = require('../db/models/Contribution');
 
 const index = async (req, res) => {
-    res.status(StatusCodes.OK).send('Casa dulce casa');
+    const response = 'Publications: \n';
+    try {
+        const posts = await Contribution.findAll({
+            where: {
+                type: 'post',
+            },
+            attributes: ['title', 'text', 'url'],
+        });
+        posts.forEach((post) => {
+            response += `${post.title} ${
+                post.text === undefined || post.text == ''
+                    ? post.url
+                    : post.text
+            } \n`;
+        });
+
+        res.status(StatusCodes.OK).send(response);
+    } catch (e) {}
 };
 
 const newest = async (req, res) => {
