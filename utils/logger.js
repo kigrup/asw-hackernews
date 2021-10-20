92a49ab5b1b7f0be9225b71be1764f61b8c33cac
Raul Plesa <raul.plesa@estudiantat.upc.edu>
2021-10-20 16:55:28 +0200

    Added log format

--- utils/logger.js
+++ utils/logger.js
@@ -1,10 +1,19 @@
 const winston = require('winston');
 const moment = require('moment');
 
+const myFormat = printf(({ level, message, timestamp, ...metadata }) => {
+    let msg = `${timestamp} [${level}] : ${message} `;
+    if (metadata) {
+        msg += JSON.stringify(metadata);
+    }
+    return msg;
+});
+
 const logger = winston.createLogger({
     format: winston.format.combine(
         winston.format.splat(),
-        winston.format.timestamp()
+        winston.format.timestamp(),
+        myFormat
     ),
     defaultMeta: { service: 'server' },
     transports: [
