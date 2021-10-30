202651388a36c96972b9a4397bca1c0cdcfe0640
Raul Plesa <raul.plesa@gmail.com>
2021-10-30 16:58:37 +0200

    Better session logs

--- db/user-management.js
+++ db/user-management.js
@@ -3,6 +3,9 @@ const utils = require('../utils/utils');
 
 const authenticateGloggedInUser = async (user) => {
     try {
+        console.log(
+            `User authenticated successfully, checking if registered...`
+        );
         // See if user is already registered
         const registeredUser = await db.users.findOne({
             where: {
@@ -10,13 +13,16 @@ const authenticateGloggedInUser = async (user) => {
             },
         });
         if (registeredUser === undefined) {
+            console.log(`User is not registered, creating new User...`);
             let email = user.emails[0].value;
             const newUser = await db.users.create({
                 id: user.id,
                 username: utils.emailToUsername(email),
                 email: email,
             });
-            console.log(`registerd user from first logged session`);
+            console.log(`New User created successfully.`);
+        } else {
+            console.log(`User was already registered.`);
         }
     } catch (e) {
         console.log(`Issues in authenticateGloggedInUser:`);
