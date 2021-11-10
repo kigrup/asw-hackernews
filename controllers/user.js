b0917098822b44c22787148780cb6a0cf892d71f
xavo7 <xavier14@hotmail.es>
2021-11-10 20:53:56 +0100

    renderObject user

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const { StatusCodes } = require('http-status-codes');
 const db = require('../db/db');
+const moment = require('moment');
 
 const user = async (req, res) => {
     try {
@@ -32,28 +33,36 @@ const user = async (req, res) => {
 
 const modify = async (req, res) => {
     try {
+        let renderObject = {
+            moment: moment,
+            loggedIn: false,
+            baseUrl: require('../utils/Constants').BASE_URL,
+            user:{}
+        };
+        const authorObject;
         if(!req.isAuthenticated()){
-            res.redirect('/login')
+            res.redirect('/login');
+            return;
         }
+        else{
+            renderObject.loggedIn = true;
+            authorObject= await db.users.findOne({
+                where: {
+                    id: req.user.id,
+                }
+            });
+            renderObject.user = authorObject;
+        }
+
         const { about } = req.body;
         
-        const authorObject = await db.users.findOne({
-            where: {
-                id: req.user.id,
-            }
-        });
-
         if (about !== undefined ) {
             authorObject.about = about;
             authorObject.save();
         }
-        const author = await db.users.findOne({
-            where: {
-                id: req.user.id,
-            }
-        });
-        console.log("authorObject.about");
-        console.log(author.about);
+
+        res.redirect(`/user?id=${id}`);
+
         
     } catch (e) {
         console.log('Error modfiying about');
