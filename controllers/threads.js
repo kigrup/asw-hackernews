0ad07d2f4e18a6e9f64716e8801c75e87148bb95
kigrup <raul.plesa@gmail.com>
2021-11-10 18:10:19 +0100

    threads work?

--- controllers/threads.js
+++ controllers/threads.js
@@ -6,95 +6,80 @@ const moment = require("moment");
 moment.updateLocale("es");
 
 const threads = async (req, res) => {
-  try {
-      var seen = [];
-    const comments = await db.contributions.findAll({
-      attributes: [
-        "id",
-        "title",
-        "type",
-        "content",
-        "upvotes",
-        "comments",
-        "author",
-        "authorName",
-        "createdAt",
-      ],
-      where: {
-        type: "comment",
-        author: req.user.id,
-      },
-      include: [db.users],
-      order: [["createdAt", "DESC"]],
-    });
+    try {
+        var seen = [];
+        const comments = await db.contributions.findAll({
+            attributes: [
+                "id",
+                "title",
+                "type",
+                "content",
+                "upvotes",
+                "comments",
+                "author",
+                "authorName",
+                "createdAt",
+            ],
+            where: {
+                type: "comment",
+                author: req.user.id,
+            },
+            include: [db.users],
+            order: [["createdAt", "DESC"]],
+        });
+        var repliesIds = [];
         const populateComments = async (commentsObject, depth) => {
-            for (
-                let i = 0;
-                i < commentsObject.length;
-                i++
-            ) 
-            {            
-            if(comments.includes(commentsObject[i]) && depth > 0)    //Eliminar comments repetidos
-            {
-                var index = comments.findIndex(comment => comment.dataValues.id == commentsObject[i].dataValues.id);
-                seen[index] = true;
-            }
+            for (let i = 0; i < commentsObject.length; i++) {
                 {
                     child = await db.contributions.findOne({
-                    where: {
-                        id: commentsObject[i].dataValues.id,
-                    },
-                    include: [db.contributions],
-                });
-                commentsObject[i] = child;
-                //console.log('CHILD:');
-                //console.log(require('util').inspect(child, false, 6, false));
-                if (child.dataValues.contributions !== undefined) 
-                    {
-                    console.log('------POPULATING COMMENT---------');
-                    console.log(child.dataValues.content);
-                    await populateComments(
-                        commentsObject[i].dataValues.contributions, depth+1
-                    );
-                    }                
+                        where: {
+                            id: commentsObject[i].dataValues.id,
+                        },
+                        include: [db.contributions],
+                    });
+                    commentsObject[i] = child;
+                    if (depth > 0) {
+                        repliesIds.push(commentsObject[i]);
+                    }
+                    //console.log('CHILD:');
+                    //console.log(require('util').inspect(child, false, 6, false));
+                    if (child.dataValues.contributions !== undefined) {
+                        console.log("------POPULATING COMMENT---------");
+                        console.log(child.dataValues.content);
+                        await populateComments(
+                            commentsObject[i].dataValues.contributions,
+                            depth + 1
+                        );
+                    }
                 }
-            }          
+            }
         };
-        for (let i = 0; i < comments.length; i++)
-        {
-            seen.push(false);
-        }
         await populateComments(comments, 0);
-        let iterator = 0;
-        while (iterator < comments.length){
-            if (seen[iterator] == true) {
-                comments.splice(iterator, 1);
-                seen.splice(iterator, 1);
-            }
-            else {
-                iterator++;
+        var uniqueComments = [];
+        for (let i = 0; i < comments.length; i++) {
+            if (repliesIds.includes(comments[i].dataValues.id)) {
+                uniqueComments.push(comments[i]);
             }
         }
+
         const post = { deep: 0 }; // dummy object to read indentation
         let renderObject = {
             post: post,
             comments: comments,
             moment: moment,
             loggedIn: false,
-            baseUrl: require('../utils/Constants').BASE_URL
+            baseUrl: require("../utils/Constants").BASE_URL,
         };
         if (req.isAuthenticated()) {
             renderObject.loggedIn = true;
             renderObject.user = req.user;
-        }        
-        res.render('pages/threads', renderObject);
+        }
+        res.render("pages/threads", renderObject);
     } catch (e) {
-        console.log('Issue in Threads');
+        console.log("Issue in Threads");
         console.log(e.message);
-        res.status(StatusCodes.OK).send('Error');
-
+        res.status(StatusCodes.OK).send("Error");
     }
- 
 };
 
 module.exports = threads;
