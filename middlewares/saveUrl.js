cc3730e688c6c3e9882bf7cc7bf244ada7dd7fd7
Raul Plesa <raul.plesa@gmail.com>
2021-11-13 14:18:34 +0100

    redirect

--- middlewares/saveUrl.js
+++ middlewares/saveUrl.js
@@ -3,7 +3,14 @@ const { StatusCodes } = require('http-status-codes');
 const saveUrl = async (req, res, next) => {
     if (req.user) {
         let url = req.url;
-        if (url != '/data/hn.js' && url != '/s.gif' && (url.length >= 5 && url.substring(0,5) != '/vote' && url.substring(0,4) != '/log')){
+        if (
+            url != '/data/hn.js' &&
+            url != '/s.gif' &&
+            url.length >= 5 &&
+            url.substring(0, 5) != '/vote' &&
+            url.substring(0, 4) != '/log' &&
+            url.substring(0, 5) != '/user'
+        ) {
             req.session.prevUrl = req.url;
             console.log(`saved url ${req.url} for ${req.user.id}`);
         }
