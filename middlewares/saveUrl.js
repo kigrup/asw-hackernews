a96150b7f67668b10773a96f93368416fd5124b7
kigrup <raul.plesa@gmail.com>
2021-11-13 12:50:06 +0100

    s

--- middlewares/saveUrl.js
+++ middlewares/saveUrl.js
@@ -3,7 +3,7 @@ const { StatusCodes } = require('http-status-codes');
 const saveUrl = async (req, res, next) => {
     if (req.user) {
         let url = req.url;
-        if (url != '/data/hn.js' && url != '/s.gif' && url.substring(0,5) != '/vote'){
+        if (url != '/data/hn.js' && url != '/s.gif' && (url.length >= 5 && url.substring(0,5) != '/vote')){
             req.session.prevUrl = req.url;
             console.log(`saved url ${req.url} for ${req.user.id}`);
         }
