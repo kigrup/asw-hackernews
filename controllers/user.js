84d0eb05cd1cdc0f610708ab4b20a7fdfd4fd27f
pere.masip <pere.masip@estudiantat.upc.edu>
2021-11-26 15:50:10 +0100

    threads and user controllerLogic ready for testing

--- controllers/user.js
+++ controllers/user.js
@@ -2,36 +2,20 @@ const { StatusCodes } = require('http-status-codes');
 const db = require('../db/db');
 const moment = require('moment');
 
+import { 
+    user as userLogic,
+    modify as modifyLogic
+ } from "../controllerLogic/user";
+
+
+ 
 const user = async (req, res) => {
     try {
         const id = req.query.id;
         if (id == undefined || !id) {
             res.send('User id undefined in query');
         }
-        const user = await db.users.findOne({
-            where: {
-                id: id,
-            },
-        });
-        let logged = '';
-        let renderObject = {
-            user: {},
-            loggedIn: false,
-            moment:moment,
-            userProfile: user,
-        }
-        if (req.user && req.user.id == id) {
-            logged = 'Logged';
-        }
-        if (req.isAuthenticated()) {
-            renderObject.loggedIn = true;
-            const loggeduser = await db.users.findOne({
-                where: {
-                    id: req.user.id,
-                }
-            });
-            renderObject.user = loggeduser;
-        }
+        let { logged, renderObject } = await userLogic(id, req);
         res.render(`pages/user${logged}`, renderObject);
 
     } catch (e) {
@@ -56,12 +40,7 @@ const modify = async (req, res) => {
             });
         }
 
-        const { about } = req.body;
-        
-        if (about !== undefined ) {
-            authorObject.about = about;
-            authorObject.save();
-        }
+        modifyLogic(req, authorObject);
 
         res.redirect(`/user?id=${req.user.id}`);
 
@@ -74,3 +53,4 @@ const modify = async (req, res) => {
 };
 
 module.exports = {user, modify};
+
