4be51b2d229055ab879dd15e7647e176d890c2f5
kigrup <raul.plesa@gmail.com>
2021-11-27 12:34:49 +0100

    Fixed threads

--- controllerLogic/threads.js
+++ controllerLogic/threads.js
@@ -1,77 +1,57 @@
-const db = require('../db/db');
-const ejs = require('ejs');
-const moment = require('moment');
+const db = require("../db/db");
+const ejs = require("ejs");
+const moment = require("moment");
 
-moment.updateLocale('es');
+moment.updateLocale("es");
 
-const populateComments = async (commentsObject, depth) => {
-    for (let i = 0; i < commentsObject.length; i++) {
-        {
-            child = await db.contributions.findOne({
-                where: {
-                    id: commentsObject[i].dataValues.id,
-                },
-                include: [db.contributions],
-            });
-            child.dataValues.deep = depth;
-            commentsObject[i] = child;
-            if (depth > 1) {
-                repliesIds.push(commentsObject[i].dataValues.id);
-            }
-            //console.log('CHILD:');
-            //console.log(require('util').inspect(child, false, 6, false));
-            if (child.dataValues.contributions !== undefined) {
-                console.log('------POPULATING COMMENT---------');
-                console.log(child.dataValues.content);
-                await populateComments(
-                    commentsObject[i].dataValues.contributions,
-                    depth + 1
-                );
-            }
-        }
-    }
-};
-
-const setIsLiked = async (user, commentsObject) => {
-    for (let i = 0; i < commentsObject.length; i++) {
-        for (let l = 0; l < user.liked.length; l++) {
-            if (
-                commentsObject[i].dataValues.id ==
-                user.liked[l].dataValues.id
-            ) {
-                commentsObject[i].dataValues.isLiked = true;
-                commentsObject[i].isLiked = true;
-            }
-        }
-        await setIsLiked(
-            user,
-            commentsObject[i].dataValues.contributions
-        );
-    }
-};
-
-const threads = async(localauthor, req) => {
+const threads = async (localauthor, req) => {
     const comments = await db.contributions.findAll({
         attributes: [
-            'id',
-            'title',
-            'type',
-            'content',
-            'upvotes',
-            'comments',
-            'author',
-            'authorName',
-            'createdAt',
+            "id",
+            "title",
+            "type",
+            "content",
+            "upvotes",
+            "comments",
+            "author",
+            "authorName",
+            "createdAt",
         ],
         where: {
-            type: 'comment',
+            type: "comment",
             author: localauthor,
         },
         include: [db.users],
-        order: [['createdAt', 'DESC']],
+        order: [["createdAt", "DESC"]],
     });
     var repliesIds = [];
-
+    const populateComments = async (commentsObject, depth) => {
+        for (let i = 0; i < commentsObject.length; i++) {
+            {
+                child = await db.contributions.findOne({
+                    where: {
+                        id: commentsObject[i].dataValues.id,
+                    },
+                    include: [db.contributions],
+                });
+                child.dataValues.deep = depth;
+                commentsObject[i] = child;
+                if (depth > 1) {
+                    repliesIds.push(commentsObject[i].dataValues.id);
+                }
+                //console.log('CHILD:');
+                //console.log(require('util').inspect(child, false, 6, false));
+                if (child.dataValues.contributions !== undefined) {
+                    console.log("------POPULATING COMMENT---------");
+                    console.log(child.dataValues.content);
+                    await populateComments(
+                        commentsObject[i].dataValues.contributions,
+                        depth + 1
+                    );
+                }
+            }
+        }
+    };
     await populateComments(comments, 1);
     var uniqueComments = [];
     for (let i = 0; i < comments.length; i++) {
@@ -89,8 +69,6 @@ const threads = async(localauthor, req) => {
         user: {},
     };
 
-
-
     let loggedUser;
     if (req.user) {
         loggedUser = await db.users.findOne({
@@ -99,19 +77,35 @@ const threads = async(localauthor, req) => {
             },
             include: [
                 {
-                    association: 'liked',
+                    association: "liked",
                     model: db.contributions,
                 },
             ],
         });
         loggedUser.displayName = req.user.displayName;
+        const setIsLiked = async (user, commentsObject) => {
+            for (let i = 0; i < commentsObject.length; i++) {
+                for (let l = 0; l < user.liked.length; l++) {
+                    if (
+                        commentsObject[i].dataValues.id ==
+                        user.liked[l].dataValues.id
+                    ) {
+                        commentsObject[i].dataValues.isLiked = true;
+                        commentsObject[i].isLiked = true;
+                    }
+                }
+                await setIsLiked(
+                    user,
+                    commentsObject[i].dataValues.contributions
+                );
+            }
+        };
         await setIsLiked(loggedUser, uniqueComments);
         renderObject.comments = uniqueComments;
         renderObject.loggedIn = true;
         renderObject.user = loggedUser;
     }
     return renderObject;
-}
+};
 
-
-module.exports = threads;
+module.exports = {threads};
