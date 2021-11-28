8857b11f694f0f1c3f00bf27d5f60ce64bf0f5a4
kigrup <raul.plesa@gmail.com>
2021-11-28 16:56:08 +0100

    Fixed middlewares

--- middlewares/apiBasicLogged.js
+++ middlewares/apiBasicLogged.js
@@ -2,7 +2,7 @@ const { StatusCodes } = require('http-status-codes');
 
 const apiSelfLogged = async (req, res, next) => {
     if(req.params.userId == undefined){
-        req.status(StatusCodes.UNAUTHORIZED).send()
+        res.status(StatusCodes.UNAUTHORIZED).send()
     }
     else {
         next();
