2f2d69f4eeb63829e5bdce0f4188ca45104cb684
kigrup <raul.plesa@gmail.com>
2021-11-28 15:42:41 +0100

    Implemented API lists enpoints

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,33 @@
 const db = require("../../db/db");
 const { StatusCodes } = require('http-status-codes');
 
+const indexLogic = require('../../controllerLogic/index.js').index;
+
 const controller = async (req, res) => {
+
+    const obj = await indexLogic(false, req);
     
-    // CONTENIDO DE PLANTILLA:
-    // BORRAR ESTO CUANDO SE HAYA IMPLEMENTADO!!
-    res.status(StatusCodes.NOT_IMPLEMENTED).send();
+    let finalObj = {
+        posts: []
+    };
+
+    for (let i = 0; i < obj.posts.length; i++) {
+        let p = obj.posts[i];
+        let postObj = {
+            id: p.id,
+            title: p.title,
+            type: p.type,
+            content: p.content,
+            upvotes: p.upvotes,
+            comments: p.comments,
+            authorId: p.author,
+            authorName: p.authorName,
+            createdAt: p.createdAt
+        };
+        finalObj.posts.push(postObj);
+    }
+
+    res.status(StatusCodes.OK).json(finalObj);
 }
 
 module.exports = controller;
--- controllers/api/newest.js
+++ controllers/api/newest.js
@@ -1,11 +1,33 @@
 const db = require("../../db/db");
 const { StatusCodes } = require('http-status-codes');
 
+const newestLogic = require('../../controllerLogic/index.js').newest;
+
 const controller = async (req, res) => {
 
-    // CONTENIDO DE PLANTILLA:
-    // BORRAR ESTO CUANDO SE HAYA IMPLEMENTADO!!
-    res.status(StatusCodes.NOT_IMPLEMENTED).send();;
+    const obj = await newestLogic(false, req);
+
+    let finalObj = {
+        posts: []
+    };
+
+    for (let i = 0; i < obj.posts.length; i++) {
+        let p = obj.posts[i];
+        let postObj = {
+            id: p.id,
+            title: p.title,
+            type: p.type,
+            content: p.content,
+            upvotes: p.upvotes,
+            comments: p.comments,
+            authorId: p.author,
+            authorName: p.authorName,
+            createdAt: p.createdAt
+        };
+        finalObj.posts.push(postObj);
+    }
+
+    res.status(StatusCodes.OK).json(finalObj);
 }
 
 module.exports = controller;
