598b0376f1d9ab752668631bdfebcfe403055ff5
xavo7 <xavier14@hotmail.es>
2021-11-28 16:18:24 +0100

    ApiControllers liked

--- controllers/api/likedComments.js
+++ controllers/api/likedComments.js
@@ -1,11 +1,33 @@
 const db = require("../../db/db");
 const { StatusCodes } = require('http-status-codes');
 
+const commentsLogic = require('../../controllerLogic/liked.js').comments;
+
 const controller = async (req, res) => {
 
-    // CONTENIDO DE PLANTILLA:
-    // BORRAR ESTO CUANDO SE HAYA IMPLEMENTADO!!
-    res.status(StatusCodes.NOT_IMPLEMENTED).send();;
+    const obj = await commentsLogic(false, req);
+    
+    let finalObj = {
+        comments: []
+    };
+
+    for (let i = 0; i < obj.posts.length; i++) {
+        let p = obj.posts[i];
+        let commentObj = {
+            id: p.id,
+            content: p.content,
+            upvotes: p.upvotes,
+            replies: p.comments,
+            authorId: p.author,
+            authorName: p.authorName,
+            createdAt: p.createdAt,
+            inReplyTo: p.inReplyTo,
+            root : p.root
+        };
+        finalObj.posts.push(commentObj);
+    }
+
+    res.status(StatusCodes.OK).json(finalObj);
 }
 
 module.exports = controller;
