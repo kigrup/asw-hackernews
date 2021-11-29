e9eaa6b50436ae61fb77599585a222414df5c31a
pere.masip <pere.masip@estudiantat.upc.edu>
2021-11-29 15:12:56 +0100

    api/itemcomment ready for testing

--- controllers/api/itemComment.js
+++ controllers/api/itemComment.js
@@ -1,11 +1,29 @@
 const db = require("../../db/db");
 const { StatusCodes } = require('http-status-codes');
 
-const controller = async (req, res) => {
+const commentLogic = require("../../controllerLogic/item.js").comment;
 
-    // CONTENIDO DE PLANTILLA:
-    // BORRAR ESTO CUANDO SE HAYA IMPLEMENTADO!!
-    res.status(StatusCodes.NOT_IMPLEMENTED).send();;
+const controller = async (req, res) => {
+    try
+    {
+        let dataObject = await commentLogic (false,req,res);
+        let finalObject = {
+            comment: {
+                id: dataObject.dataValues.id,
+                type: dataObject.dataValues.type,
+                content: dataObject.dataValues.content,
+                author: dataObject.dataValues.authorName,
+                upvotes: dataObject.dataValues.upvotes,
+                createdAt: dataObject.dataValues.createdAt
+            }
+        };
+        res.status(StatusCodes.CREATED).json(finalObject);
+    }
+    catch (e) {
+        console.log('Error creating comment');
+        console.log(e.message);
+        res.status(StatusCodes.BAD_REQUEST).json({error: 'Bad request'});
+    }  
 }
 
 module.exports = controller;
